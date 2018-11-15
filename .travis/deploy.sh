f37a96049efcfa2c515188f7a9fb0ff18ce5c6d0
Mikhail K. (iSm1le) <ism1le@xaked.com>
2018-11-15 15:10:47 +0200

    reverted

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -10,10 +10,10 @@ ssh deploy@$IP -p $PORT <<EOF
   mkdir $TEMP_DEPLOY_DIR
   cd $TEMP_DEPLOY_DIR
   git clone -b live https://github.com/Xaked/static-content.git .
-  rm -rf .git
-  rm -rf .travis
-  rm -rf .travis.yml
-  rm -rf .gitignore
+  rm -rf $TEMP_DEPLOY_DIR/.git
+  rm -rf $TEMP_DEPLOY_DIR/.travis
+  rm -rf $TEMP_DEPLOY_DIR/.travis.yml
+  rm -rf $TEMP_DEPLOY_DIR/.gitignore
   chmod -R u=rwX,g=rwX,o=rX *
   rsync -au --delete "$TEMP_DEPLOY_DIR/" "$DEPLOY_DIR/"
 EOF
